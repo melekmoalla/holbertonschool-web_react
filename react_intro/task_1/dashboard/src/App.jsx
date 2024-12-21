f572b5ee3955777927032faa2b8eaf2ed4b4369c
melekmoalla <melekmoalla2468@gmail.com>
2024-12-21 22:02:34 +0100

    a new task with new info

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/holberton-logo.jpg';
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+  return (
+    <div>
+      <div className='root-notifications'>
+          <Notifications />
+      </div>
+      <header className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+      </header>
+
+      <div className="App-body">
+        <div className="long-br"></div>
+          <p>Login to access the full dashboard</p>
+        </div>
+
+        <footer className="App-footer">
+        <div className="long-br"></div>
+        <p>{getFooterCopy(true)}</p>
+        <p>{getCurrentYear()}</p>
+        </footer>
+      </div>
+  );
+}
+
+export default App;
