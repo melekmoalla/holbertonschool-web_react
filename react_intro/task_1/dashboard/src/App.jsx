dac4c9226e5f263b6ae94f73cbbd6a4c0ed000f2
melekmoalla <melekmoalla2468@gmail.com>
2024-12-22 12:45:54 +0100

    task

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -14,16 +14,22 @@ function App() {
         <img src={logo} alt="holberton logo" />
         <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </header>
-
+      
       <div className="App-body">
         <div className="long-br"></div>
-          <p>Login to access the full dashboard</p>
+        <p>Login to access the full dashboard</p>
+        <form>
+          <div>
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" />
+          </div>
+          <div>
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" />
+          </div>
           <button type="submit">OK</button>
-        </div>
+        </form>
+      </div>
 
         <footer className="App-footer">
         <div className="long-br"></div>
