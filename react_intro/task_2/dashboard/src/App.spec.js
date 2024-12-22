d64695dba1a4139de31825a423efb4595af76ea9
melekmoalla <melekmoalla2468@gmail.com>
2024-12-22 13:29:14 +0100

    test task 4 .jest

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -8,7 +8,7 @@ test('renders h1 element with text "School Dashboard"', () => {
     const h1Element = screen.getByRole('heading', { level: 1, name: /School Dashboard/i });
     expect(h1Element).toBeInTheDocument();
 })
-
+/*
 test('Write a test to check that the text content within the 2 p elements in the app-body and app-footer divs matches the text shown in the previous task screenshot.', () =>{
     render(<App />);
     
@@ -19,9 +19,35 @@ test('Write a test to check that the text content within the 2 p elements in the
     expect(bodyText).toBeInTheDocument();
     expect(footerText).toBeInTheDocument();
 });
-
+*/
 test('test to check if an img element is rendered.', () => {
     render(<App />);
     const image = screen.getByAltText("holberton logo");
     expect(image).toBeInTheDocument();
 })
+
+test('renders two input elements', () => {
+    render(<App />);
+
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    expect(emailInput).toBeInTheDocument();
+  
+    const passwordInput = screen.getByLabelText(/password/i);
+    expect(passwordInput).toBeInTheDocument();
+
+});
+
+test('renders two label elements with Email and Password text', () => {
+    render(<App />);
+    const emailLabel = screen.getByLabelText(/email/i);
+    const passwordLabel = screen.getByLabelText(/password/i);
+    
+    expect(emailLabel).toBeInTheDocument();
+    expect(passwordLabel).toBeInTheDocument();
+    });
+
+test('renders a button with text "OK"', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /ok/i });
+    expect(button).toBeInTheDocument();
+    });
