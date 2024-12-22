1c0fae8b49f61e1e78ebbf261766af0ea1359357
melekmoalla <melekmoalla2468@gmail.com>
2024-12-22 15:16:06 +0100

    test

--- react_intro/task_3/dashboard/src/App.spec.js
+++ react_intro/task_3/dashboard/src/App.spec.js
@@ -9,18 +9,16 @@ test('renders h1 element with text "School Dashboard"', () => {
     const h1Element = screen.getByRole('heading', { level: 1, name: /School Dashboard/i });
     expect(h1Element).toBeInTheDocument();
 })
-/*
 test('Write a test to check that the text content within the 2 p elements in the app-body and app-footer divs matches the text shown in the previous task screenshot.', () =>{
     render(<App />);
     
-    // Update the expected text to match what is in the component
     const bodyText = screen.getByText("Login to access the full dashboard"); 
-    const footerText = screen.getByText("Copyright 2024 - holberton School");
+    const footerText = screen.getByText("Copyright 2024 Holberton School");
   
     expect(bodyText).toBeInTheDocument();
     expect(footerText).toBeInTheDocument();
 });
-*/
+
 test('test to check if an img element is rendered.', () => {
     render(<App />);
     const image = screen.getByAltText("holberton logo");
