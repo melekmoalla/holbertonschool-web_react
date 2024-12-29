f4843f89779f2ec108ca4b41dd3fc8e1131ccc68
melekmoalla <melekmoalla2468@gmail.com>
2024-12-29 21:10:54 +0100

    test now

--- react_props/task_4/dashboard/src/CourseList/CourseList.spec.js
+++ react_props/task_4/dashboard/src/CourseList/CourseList.spec.js
@@ -4,11 +4,11 @@ import CourseList from './CourseList';
 
 describe('CourseList Component', () => {
   it('renders 5 rows when courses array is provided', () => {
-    const coursesList = [
+        const coursesList = [
         { id: 1, name: "ES6", credit: 60 },
         { id: 2, name: "Webpack", credit: 20 },
         { id: 3, name: "React", credit: 40 },
-      ];
+        ];
     render(<CourseList courses={courses} />);
     const rows = screen.getAllByRole('row');
     expect(rows).toHaveLength(5); // 2 headers + 3 courses
--- react_props/task_4/dashboard/src/CourseList/CourseListRow.spec.js
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.spec.js
@@ -3,7 +3,22 @@ import { render, screen } from '@testing-library/react';
 import CourseListRow from './CourseListRow';
 
 describe('test compnement',() =>{
-    test('should pass this test', () => {
-        expect(true).toBe(true);
+    it('renders a single column header with colSpan=2 when isHeader is true and textSecondCell is null', () => {
+        render(<CourseListRow isHeader={true} textFirstCell="Test Header" />);
+        const thElement = screen.getByText('Test Header');
+        expect(thElement).toBeInTheDocument();
+        expect(thElement).toHaveAttribute('colSpan', '2');
+      });
+    
+      it('renders two column headers when isHeader is true and textSecondCell is not null', () => {
+        render(<CourseListRow isHeader={true} textFirstCell="Header1" textSecondCell="Header2" />);
+        expect(screen.getByText('Header1')).toBeInTheDocument();
+        expect(screen.getByText('Header2')).toBeInTheDocument();
+      });
+    
+      it('renders two data cells when isHeader is false', () => {
+        render(<CourseListRow isHeader={false} textFirstCell="Data1" textSecondCell="Data2" />);
+        expect(screen.getByText('Data1')).toBeInTheDocument();
+        expect(screen.getByText('Data2')).toBeInTheDocument();
       });
     });
