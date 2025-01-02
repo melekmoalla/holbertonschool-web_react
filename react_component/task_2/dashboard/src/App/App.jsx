201b3583608d08cbd4b13ed85ab4ff3f6e54ad69
melekmoalla <melekmoalla2468@gmail.com>
2025-01-02 18:56:39 +0100

    task 2 test

---
+++ react_component/task_2/dashboard/src/App/App.jsx
@@ -0,0 +1,71 @@
+import React, { Fragment , Component } from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+
+const notificationsList = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  { id: 3, type: "urgent", html: { __html: "<strong>Urgent requirement</strong> - complete by EOD" } },
+];
+
+const coursesList = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+  
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  }
+
+  render() {
+    const isLoggedIn = false;
+
+  return (
+    <Fragment>
+      <div className="root-notifications">
+      <Notifications notifications={notificationsList} displayDrawer={true} />
+      </div>
+      <Header />
+      <div className="body-content">
+        {isLoggedIn ? (
+          <CourseList courses={coursesList} />
+        ) : (
+          <Login />
+        )}
+      </div>
+      <Footer />
+    </Fragment>
+  );
+  }
+}
+
+App.defaultProps = {
+  logOut: () => {},
+};
+
+
+export default App;
