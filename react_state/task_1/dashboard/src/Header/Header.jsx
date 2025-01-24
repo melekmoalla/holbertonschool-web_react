de8a587014aea281df6b5acecf078b100fbef4e9
melekmoalla <melekmoalla2468@gmail.com>
2025-01-24 16:48:57 +0100

    task 1

---
+++ react_state/task_1/dashboard/src/Header/Header.jsx
@@ -0,0 +1,30 @@
+// src/Header/Header.jsx
+import React from 'react';
+import logo from '../assets/holberton-logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+
+const Header = () => {
+  return (
+    <header className={css(styles.Appheader)}>
+      <img src={logo} alt="holberton logo" />
+      <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+    </header>
+  );
+};
+
+const styles = StyleSheet.create({
+  Appheader: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  
+  headerimg: {
+    marginRight: '10px',
+  },
+  
+  headerh1: {
+    color: '#e1003c',
+  },
+});
+
+export default Header;
