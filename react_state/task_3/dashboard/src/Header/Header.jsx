90e2a03b0046e60a0a685219696bc657f190530e
melekmoalla <melekmoalla2468@gmail.com>
2025-01-26 17:06:41 +0100

    test task

--- react_state/task_3/dashboard/src/Header/Header.jsx
+++ react_state/task_3/dashboard/src/Header/Header.jsx
@@ -1,20 +1,35 @@
 // src/Header/Header.jsx
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/holberton-logo.jpg';
 import { StyleSheet, css } from 'aphrodite';
 import newContext from '../Context/context';
 
 
 
-const Header = () => {
-  
-  return (
+class Header extends Component {
+  static contextType = newContext;
+
+  render() {
+    const { isLoggedIn, user, logOut } = this.context; // Destructure context
+
+    return (
+
     <header className={css(styles.Appheader)}>
       <img src={logo} alt="holberton logo" />
       <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
+      {isLoggedIn && (
+        
+          <section id="logoutSection">
+            <p>
+              Welcome {user.email} <button onClick={logOut}>Logout</button>
+            </p>
+          </section>
+        )}
     </header>
-  );
-};
+    
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   Appheader: {
