c1135900045b6116659f66d659801b693385493d
melekmoalla <melekmoalla2468@gmail.com>
2025-02-05 11:58:31 +0100

    task3 for folder 2

---
+++ react_hooks/task_2/dashboard/src/Login/Login.jsx
@@ -0,0 +1,122 @@
+// src/Login/Login.jsx
+import React, { useState } from 'react';
+import WithLogging from '../HOC/WithLogging';
+import { StyleSheet, css } from 'aphrodite';
+
+const Login = (props) => {
+
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
+  handleLoginSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = formData;
+    this.props.logIn(email, password);
+  }
+  
+   handleChangeEmail = (event) => {
+    const newEmail = event.target.value;
+    setFormData((prevData) => {
+      const updatedData = { ...prevData, email: newEmail };
+      validateForm(updatedData);
+      return updatedData;
+    });
+  };
+
+   handleChangePassword = (event) => {
+    const newPassword = event.target.value;
+    setFormData((prevData) => {
+      const updatedData = { ...prevData, password: newPassword };
+      validateForm(updatedData);
+      return updatedData;
+    });
+  };
+
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regex for email validation
+    const isEmailValid = emailRegex.test(email);
+    const isPasswordValid = password.length >= 8;
+    setEnableSubmit(isEmailValid && isPasswordValid);
+  }
+
+    const { email, password } = formData;
+
+
+    return (
+      <div className={css(styles.Appbody)}>
+        <div className={css(styles.longbr)}></div>
+        <p>Login to access the full dashboard</p>
+        <form
+          className={css(styles.Appbodyform)}
+          onSubmit={this.handleLoginSubmit}
+        >
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={this.handleChangeEmail}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={this.handleChangePassword}
+          />
+          <input
+            type="submit"
+            value="OK"
+            disabled={!enableSubmit}
+            className={css(styles.button, !enableSubmit && styles.disabled)}
+          />
+        </form>
+      </div>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  Appbody: {
+    marginTop: '20px',
+  },
+
+  longbr: {
+    borderTop: '1px solid red',
+    marginBottom: '20px',
+  },
+  
+  Appbodyform: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    marginTop: '20px',
+  },
+  
+  button: {
+    marginTop: '15px',
+    padding: '10px',
+    fontSize: '16px',
+    backgroundColor: '#007bff',
+    color: 'white',
+    border: 'none',
+    cursor: 'pointer',
+    '@media (max-width: 900px)': {
+      fontSize: '14px',
+    },
+    ':hover': {
+      backgroundColor: '#0056b3',
+    },
+  }, 
+  disabled: {
+    backgroundColor: "#c0c0c0",
+    cursor: "not-allowed",
+  },
+});
+
+
+export default WithLogging(Login);
---
+++ react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -0,0 +1,36 @@
+// src/Notifications/NotificationItem.jsx
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+function NotificationItem({ id, type, html, value, markAsRead }) {
+
+  const handleClick = () => {
+    markAsRead(id);
+  };
+
+  return (
+    <li
+      data-notification-type={type}
+      style={{ color: type === 'urgent' ? 'red' : 'blue' }}
+      dangerouslySetInnerHTML={html ? html : undefined}
+      onClick={handleClick}
+    >
+      {html ? null : value}
+    </li>
+  );
+}
+
+NotificationItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  html: PropTypes.object,
+  markAsRead: PropTypes.func.isRequired,
+};
+
+NotificationItem.defaultProps = {
+  value: '',
+  html: null,
+};
+
+export default memo(NotificationItem);
