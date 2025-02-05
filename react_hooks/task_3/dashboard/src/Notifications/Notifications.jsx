a8d12831e1e4dac3b62d217d96fbb6e4f2adc423
melekmoalla <melekmoalla2468@gmail.com>
2025-02-05 13:25:37 +0100

    test

--- react_hooks/task_3/dashboard/src/Notifications/Notifications.jsx
+++ react_hooks/task_3/dashboard/src/Notifications/Notifications.jsx
@@ -1,44 +1,49 @@
-import React, { memo } from 'react';
-import PropTypes from 'prop-types';
-import NotificationItem from './NotificationItem';
-import { StyleSheet, css } from 'aphrodite';
+import React, { memo } from "react";
+import NotificationItem from "./NotificationItem";
+import PropTypes from "prop-types";
+import closeIcon from "../assets/close-button.png";
+import { StyleSheet, css } from "aphrodite";
 
 const Notifications = ({
   displayDrawer,
   listNotifications,
   handleDisplayDrawer,
   handleHideDrawer,
-  markNotificationAsRead,
+  markAsRead,
 }) => {
   return (
-    <>
-      <div className="menuItem" onClick={handleDisplayDrawer}>
-        Your notifications
-      </div>
-      {displayDrawer && (
-        <div className="Notifications">
-          <button
-            style={{
-              position: 'absolute',
-              right: '1rem',
-              top: '1rem',
-              background: 'none',
-              border: 'none',
-              cursor: 'pointer',
-            }}
-            aria-label="Close"
-            onClick={handleHideDrawer}
-          >
-            <img src="close-icon.png" alt="close icon" />
-          </button>
-          <p>Here is the list of notifications</p>
-          <ul>
+      <>
+        <div className={css(styles.menuItem)} id="menuItem">
+          <p onClick={handleDisplayDrawer} className={menuPStyle}>Your notifications</p>
+        </div>
+        {displayDrawer && (
+          <div className={css(styles.notifications)} id="Notifications">
+            <button
+              style={{
+                background: "transparent",
+                border: "none",
+                position: "absolute",
+                right: 20,
+              }}
+              onClick={handleHideDrawer}
+              aria-label="close"
+            >
+              <img
+                src={closeIcon}
+                alt="close-icon"
+                className={css(styles.notificationsButtonImage)}
+              />
+            </button>
+            <p className={css(styles.notificationsP)}>
+              Here is the list of notifications
+            </p>
+            <ul>
             {listNotifications.length === 0 ? (
               <NotificationItem
                 id={0}
                 type="default"
                 value="No new notification for now"
-                markAsRead={markNotificationAsRead}
+                markAsRead={markAsRead}
               />
             ) : (
               listNotifications.map((notification) => (
@@ -48,7 +53,7 @@ const Notifications = ({
                   type={notification.type}
                   value={notification.value}
                   html={notification.html}
-                  markAsRead={markNotificationAsRead}
+                  markAsRead={markAsRead}
                 />
               ))
             )}
@@ -56,8 +61,8 @@ const Notifications = ({
         </div>
       )}
     </>
-  );
-};
+    );
+  }
 
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
@@ -83,18 +88,117 @@ Notifications.defaultProps = {
   handleHideDrawer: () => {},
 };
 
+
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const screenSize = {
+  small: "@media screen and (max-width: 900px)",
+};
+
+const opacityKeyframes = {
+  from: {
+    opacity: 0.5,
+  },
+
+  to: {
+    opacity: 1,
+  },
+};
+
+const translateYKeyframes = {
+  "0%": {
+    transform: "translateY(0)",
+  },
+
+  "50%": {
+    transform: "translateY(-5px)",
+  },
+
+  "75%": {
+    transform: "translateY(5px)",
+  },
+
+  "100%": {
+    transform: "translateY(0)",
+  },
+};
+
+const borderKeyframes = {
+  "0%": {
+    border: `3px dashed deepSkyBlue`,
+  },
+
+  "100%": {
+    border: `3px dashed ${cssVars.mainColor}`,
+  },
+};
+
 const styles = StyleSheet.create({
   menuItem: {
-    textAlign: 'right',
+    float: "right",
+    backgroundColor: "#fff8f8",
+    ":hover": {
+      cursor: "pointer",
+      animationName: [opacityKeyframes, translateYKeyframes],
+      animationDuration: "1s, 0.5s",
+      animationIterationCount: 3,
+    },
   },
-  Notifications: {
-    border: '1px dashed #e1354b',
-    padding: '1rem',
-    position: 'absolute',
-    right: '1rem',
-    top: '1.5rem',
-    width: '300px',
-    backgroundColor: 'white',
+
+  menuItemPNoShow: {
+    marginRight: "8px",
+    display: "none",
+  },
+
+  menuItemPShow: {
+    marginRight: "8px",
+  },
+
+  notifications: {
+    float: "right",
+    // border: `3px dashed ${cssVars.mainColor}`,
+    padding: "10px",
+    marginBottom: "20px",
+    animationName: [borderKeyframes],
+    animationDuration: "0.8s",
+    animationIterationCount: 1,
+    animationFillMode: "forwards",
+    ":hover": {
+      border: `3px dashed deepSkyBlue`,
+      // animationFillMode: "forwards",
+    },
+    [screenSize.small]: {
+      float: "none",
+      border: "none",
+      listStyle: "none",
+      padding: 0,
+      fontSize: "20px",
+      ":hover": {
+        border: "none",
+        // animationFillMode: "forwards",
+      },
+      position: "absolute",
+      background: "white",
+      height: "110vh",
+      width: "100vw",
+    },
+  },
+
+  notificationsButtonImage: {
+    width: "10px",
+  },
+
+  notificationsP: {
+    margin: 0,
+    marginTop: "15px",
+  },
+
+  notificationsUL: {
+    [screenSize.small]: {
+      padding: 0,
+    },
   },
 });
 
