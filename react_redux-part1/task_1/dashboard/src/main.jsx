b22bae76738acf2d861646029e3e41da20b9d429
melekmoalla <melekmoalla2468@gmail.com>
2025-02-07 17:43:07 +0100

    test task 1

---
+++ react_redux-part1/task_1/dashboard/src/main.jsx
@@ -0,0 +1,13 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import authReducer from './features/auth/authSlice';
+import notificationsReducer from './features/notifications/notificationsSlice';
+import coursesReducer from './features/courses/coursesSlice';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  notifications: notificationsReducer,
+  courses: coursesReducer,
+  ui: uiReducer
+});
+
+export default rootReducer;
