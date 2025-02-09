9993f78201a1c28d2cbc9ede5604d44d5ec58ef5
melekmoalla <melekmoalla2468@gmail.com>
2025-02-09 16:12:57 +0100

    task 7

--- react_redux-part1/task_3/dashboard/src/components/Footer/Footer.jsx
+++ react_redux-part1/task_3/dashboard/src/components/Footer/Footer.jsx
@@ -1,24 +1,53 @@
-import React, { useContext } from 'react';
-import './Footer.css';
-import { getCurrentYear, getFooterCopy } from '../../utils/utils';
-import newContext from '../../Context/context';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { StyleSheet, css } from 'aphrodite';
+import { getFooterCopy, getCurrentYear } from '../../utils/utils';
 
 const Footer = () => {
-  const { user } = useContext(newContext);
+  // Get isLoggedIn state from Redux store
+  const { isLoggedIn } = useSelector(state => state.auth);
 
   return (
-    <footer className="App-footer">
-      <div className="long-br"></div>
-      <p>
-        Copyright {getCurrentYear()} {getFooterCopy(true)}
+    <footer className={css(styles.footer)} data-testid="footer">
+      <p className={css(styles.copyright)}>
+        Copyright {getCurrentYear()} - {getFooterCopy(true)}
       </p>
-      {user?.isLoggedIn && (
-        <p>
-          <a href="#">Contact us</a>
+      {isLoggedIn && (
+        <p className={css(styles.contact)}>
+          <a href="#" className={css(styles.contactLink)}>
+            Contact us
+          </a>
         </p>
       )}
     </footer>
   );
 };
+
+const styles = StyleSheet.create({
+  footer: {
+    borderTop: '3px solid #e1354b',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'fixed',
+    bottom: 0,
+    backgroundColor: 'white',
+    padding: '1rem 0',
+  },
+  copyright: {
+    margin: 0,
+  },
+  contact: {
+    margin: '0.5rem 0 0 0',
+  },
+  contactLink: {
+    color: '#e1354b',
+    textDecoration: 'none',
+    ':hover': {
+      textDecoration: 'underline',
+    },
+  },
+});
 
 export default Footer;
