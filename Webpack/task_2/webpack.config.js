933b214429bb525f9d6139cce5566f384389e2bf
melekmoalla <melekmoalla2468@gmail.com>
2025-02-10 16:09:34 +0100

    task 2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: { progressive: true },
+              optipng: { optimizationLevel: 5 },
+              gifsicle: { interlaced: false },
+              webp: { quality: 75 },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
